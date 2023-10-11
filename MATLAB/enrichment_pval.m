00c3671877287ea457137211dd2ac0bdbfd27fe2
huilisabrina <hui_li@g.harvard.edu>
2023-10-11 16:54:59 -0400

    Update enrichment_pval.m

--- MATLAB/enrichment_pval.m
+++ MATLAB/enrichment_pval.m
@@ -27,7 +27,7 @@ function [pval] = enrichment_pval(h2, h2SE, h2Cov, p_vec, varargin)
     T = h2 ./ p_vec - h2(refCol) / p_vec(refCol);
     var_vec = diag(h2Cov)';
     SE = sqrt(var_vec ./ (p_vec.^2) + var_vec(refCol) ./ (p_vec(refCol)^2) - ...
-        2 * var_vec(refCol,:) ./ p_vec / p_vec(refCol));
+        2 * h2Cov(refCol,:) ./ p_vec / p_vec(refCol));
     Z_stat = T ./ SE;
     Z_stat(1) = h2(1) / h2SE(1);
     pval = 2 * normcdf(abs(Z_stat), 'upper');
